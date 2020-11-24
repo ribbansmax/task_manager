5c836e55993843e2f2eb975fb70ba00bfea684c1
Max Ribbans <ribbans.max@gmail.com>
2020-11-24 12:31:01 -0500

    Add new tasks page and begin task functionality

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,21 @@
+# app/controllers/tasks_controller.rb
+
+class TasksController < ApplicationController
+  def index
+    @tasks = ['Task 1', 'Task 2', 'Task 3']
+  end
+
+  def new
+  end
+
+  def create
+    task = Task.new({
+      title: params[:task][:title],
+      description: params[:task][:description]
+      })
+
+    task.save
+
+    redirect_to '/tasks'
+  end
+end
