d034cc809c40678dd78616ee9722a07785f3338a
Max Ribbans <ribbans.max@gmail.com>
2020-11-25 14:00:46 -0500

    Finish repo

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,7 +2,7 @@
 
 class TasksController < ApplicationController
   def index
-    @tasks = ['Task 1', 'Task 2', 'Task 3']
+    @tasks = Task.all
   end
 
   def new
@@ -13,9 +13,30 @@ class TasksController < ApplicationController
       title: params[:task][:title],
       description: params[:task][:description]
       })
-
     task.save
+    redirect_to '/tasks'
+  end
 
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    task = Task.find(params[:id])
+    task.update({
+      title: params[:task][:title],
+      description: params[:task][:description]
+    })
+    task.save
+    redirect_to "/tasks/#{task.id}"
+  end
+
+  def destroy
+    Task.destroy(params[:id])
     redirect_to '/tasks'
   end
 end
